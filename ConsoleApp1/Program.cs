92ecc80dfca8fcb3e22b19f7c2b9c8107ee46240
Ali Aliyev <tu4pw6wq5@aztu.local>
2022-03-10 14:42:40 +0400

    added task1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1,2,3,4 };
+                      
+            MinMaxMarge(array);
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i]+",");
+            }
+        }
+        static void MinMaxMarge(int[] numbers)
+        {
+            int minIndex = 0;
+            int maxIndex = 0;
+            for (int i = 0; i <numbers.Length; i++)
+            {
+                if (numbers[minIndex] > numbers[i])
+                {
+                    minIndex = i;
+                }
+                if (numbers[maxIndex] < numbers[i]) 
+                { 
+                
+                    maxIndex = i;
+                }
+                
+            }
+            numbers[maxIndex] = numbers[maxIndex] + numbers[minIndex];
+            numbers[minIndex] = numbers[maxIndex] - numbers[minIndex];
+            numbers[maxIndex] = numbers[maxIndex] - numbers[minIndex];
+
+
+        }
+
+    }
+}
